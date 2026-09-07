21d14ec4d2d93eb0c5b6bdfa46cb4f86afa771f5
hakim <haka074@gmail.com>
2026-09-07 14:50:09 +0200

    route par default

---
+++ app/routers/index.php
@@ -0,0 +1,18 @@
+
+<?php
+
+// route Posts.show
+// pattern: /posts/id/slug
+//url ?posts=show&id=x
+// router posts
+
+
+
+// ROUTE PAR DÉFAUT: Les 10 derniers posts
+// PATTERN: /
+// URL: ?
+// CTRL: postsController
+// ACTION: index
+
+include_once '../app/controllers/postsController.php';
+\App\Controllers\PostsController\indexAction($connexion);
